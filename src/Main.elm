05f9e047f12d660941da5bc280844e2295016d54
Viktor Carlèn <vibo92@hotmail.com>
2024-06-02 13:23:40 +0200

    WSIP

--- src/Main.elm
+++ src/Main.elm
@@ -2,31 +2,43 @@ module Main exposing (..)
 import Html exposing (Html, div)
 import Browser
 import Dict exposing (Dict)
-
+import Http
+import Json.Decode as Decode exposing (Decoder, decodeString)
+import Json.Decode.Pipeline exposing (required, optional, hardcoded)
+import Platform.Cmd as Cmd
 
 type alias TournamentSummary = 
     { id : Int 
     , name : String
     }
 
+type alias Bet = 
+  { id: Int
+  , bet : String
+  , matchId : Int
+  , userId : Int }
+
 type alias Tournament =
   { id : Int
-  , elimination :Dict String EliminationStage
+  , bets : Dict String Bet
+  , eliminationRounds : Dict String EliminationStage
   , groups : Dict String Group
+  , matches : Dict String Match
   , name : String
   , stage : String
   , teams : Dict String Team
+  , users : Dict String User
   }
 
 type alias EliminationStage = 
   { name : String
-  , matches : Dict String Match
+  , matches : List Int
   }
 
 type alias Group  = 
   { name : String
-  , matches : Dict String Match
-  , teams : Dict String Team 
+  , matches : List Int
+  , teams : List Int 
   }
 
 type alias Team  = 
@@ -56,26 +68,111 @@ type alias User =
   , sub : String
   }
 
+type alias Score =
+  { id : Int 
+  , score : Int
+  }
+
+type alias Highscore =
+  Dict String Score
 
 type alias Model = 
-    { }
+  { tournaments : Dict String Tournament }
+
 
 type Msg
-    = NoOp
+    = RequestTournaments
+    | ReceieveTournaments (Result Http.Error (Dict String Tournament))
+
+
+betsDecoder : Decoder Bet
+betsDecoder =
+  Decode.map4 Bet
+    (Decode.field "id" Decode.int )
+    (Decode.field "bet" Decode.string)
+    (Decode.field "matchId" Decode.int)
+    (Decode.field "userId" Decode.int)
+
+matchDecoder : Decoder Match
+matchDecoder = 
+  Decode.succeed Match
+    |> required "id" Decode.int
+    |> required "awayScore" Decode.int
+    |> required "awayTeamId" Decode.int
+    |> required "date" Decode.string
+    |> required "groupId" Decode.int
+    |> required "homeScore" (Decode.list Decode.int)
+    |> required "homeTeamId" Decode.int
+    |> required "outcome" Decode.string
+    |> required "status" Decode.string
+    |> required "tournamentId" Decode.int
+    |> required "stageId" Decode.int
+
+eliminationStageDecoder : Decoder EliminationStage
+eliminationStageDecoder =
+  Decode.map2 EliminationStage
+    (Decode.field "name" Decode.string)
+    (Decode.field "matches" (Decode.list Decode.int))
+
+groupDecoder : Decoder Group
+groupDecoder = 
+  Decode.map3 Group
+    (Decode.field "name" Decode.string)
+    (Decode.field "matches" (Decode.list Decode.int))
+    (Decode.field "teams" (Decode.list Decode.int))
+
+teamDecoder : Decoder Team
+teamDecoder =
+  Decode.map3 Team
+    (Decode.field "id" Decode.int)
+    (Decode.field "name" Decode.string)
+    (Decode.field "logo" Decode.string)
+
+userDecoder : Decoder User
+userDecoder =
+  Decode.map3 User
+    (Decode.field "id" Decode.int)
+    (Decode.field "name" Decode.string)
+    (Decode.field "sub" Decode.string)
+
+tournamentDecoder : Decoder Tournament
+tournamentDecoder =
+  Decode.succeed Tournament
+    |> required "id" Decode.int
+    |> required "bets" (Decode.dict betsDecoder)
+    |> required "eliminationRounds" (Decode.dict eliminationStageDecoder)
+    |> required "groups" (Decode.dict groupDecoder)
+    |> required "matches" (Decode.dict matchDecoder)
+    |> required "name" Decode.string
+    |> required "stage" Decode.string
+    |> required "teams" (Decode.dict teamDecoder)
+    |> required "users" (Decode.dict userDecoder)
+
+fetchTournaments : Cmd Msg
+fetchTournaments =
+  Http.get 
+    { url = ""
+    , expect = Http.expectJson ReceieveTournaments (Decode.dict tournamentDecoder) }
 
 main : Program () Model Msg
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.element { init = init, update = update, subscriptions = always Sub.none, view = view }
 
 init : Model
 init =
-    { }
+    { tournaments = Dict.empty }
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    case msg of
-        NoOp ->
-            model
+  case msg of
+    RequestTournaments ->
+      (model, fetchTournaments)
+
+    ReceieveTournaments (Ok tournaments) ->
+      ({model | tournaments = tournaments }, Cmd.none)
+    
+    ReceieveTournaments (Err _) ->
+      (model, Cmd.none)
 
 view : Model -> Html Msg
 view model =
